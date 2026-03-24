71e2f094f2444fcd7beb493f1411a07921310d0c
Adkhambek <adhambekhusinboyev@gmail.com>
2026-03-24 11:51:05 +0500

    referallar yaxshilandi

--- utils/excel_parser.py
+++ utils/excel_parser.py
@@ -21,10 +21,10 @@ except ImportError:
     raise ImportError("openpyxl o'rnatilmagan. pip install openpyxl")
 
 # Loyiha ildizi (utils/ ning parenti)
-ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+ROOT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 EXCEL_FILES = [
-    os.path.join(ROOT_DIR, "utils", "Fanlar_majmuasi_2025-2026.xlsx"),
+    os.path.join(ROOT_DIR, "Fanlar_majmuasi_2025-2026.xlsx"),
 ]
 
 SUBJECT_MAP = {
